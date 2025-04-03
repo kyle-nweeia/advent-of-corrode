d6b2e180a4909324c24f970957be845474a4cbaf
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-04-03 11:38:54 -0600

    Add axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    axum::serve(
+        TcpListener::bind("localhost:3000").await.unwrap(),
+        axum::Router::new().route("/", axum::routing::get(async || "Hello, world!")),
+    )
+    .await
+    .unwrap();
 }
