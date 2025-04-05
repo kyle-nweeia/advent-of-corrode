7b1ea00d4ee8f6320202a0643a9fe62ada67cfa9
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-04-05 11:25:52 -0600

    Return TcpListener errors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use tokio::net::TcpListener;
-
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> std::io::Result<()> {
     axum::serve(
-        TcpListener::bind("localhost:3000").await.unwrap(),
+        tokio::net::TcpListener::bind("localhost:3000").await?,
         axum::Router::new().route("/{year}/{day}", axum::routing::get(advent_of_code::handler)),
     )
     .await
