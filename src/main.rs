b0c7672ce9373963be459f398823a66b1d2c21ea
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-04-05 10:56:26 -0600

    Return Result from main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
     axum::serve(
         TcpListener::bind("localhost:3000").await.unwrap(),
         axum::Router::new().route("/{year}/{day}", axum::routing::get(advent_of_code::handler)),
     )
     .await
-    .unwrap();
 }
