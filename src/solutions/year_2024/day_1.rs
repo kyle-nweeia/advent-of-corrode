02088554df96c0344cc5b2e24b90c15c940d9fd8
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-04-14 13:58:13 -0600

    Solve 2024/day/1

--- src/solutions/year_2024/day_1.rs
+++ src/solutions/year_2024/day_1.rs
@@ -1,5 +1,31 @@
 pub fn solve(input: String) -> f64 {
-    f64::from(11)
+    let lines: Vec<_> = input.split('\n').filter(|line| !line.is_empty()).collect();
+    let rows: Vec<_> = lines
+        .into_iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<u32>().unwrap())
+                .collect()
+        })
+        .collect();
+    let mut cols = transpose(rows);
+
+    cols.iter_mut().for_each(|col| col.sort_unstable());
+
+    transpose(cols)
+        .iter()
+        .map(|pair| pair[0].abs_diff(pair[1]))
+        .sum::<u32>()
+        .into()
+}
+
+fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let n = v[0].len();
+    let mut rows: Vec<_> = v.into_iter().map(|row| row.into_iter()).collect();
+
+    (0..n)
+        .map(|_| rows.iter_mut().map(|row| row.next().unwrap()).collect())
+        .collect()
 }
 
 #[cfg(test)]
