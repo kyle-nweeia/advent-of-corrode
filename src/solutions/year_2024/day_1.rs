610f8d624d0b1b53792c29c32bf872b69dc7beda
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-04-15 21:07:30 -0600

    Add solve_part_2

--- src/solutions/year_2024/day_1.rs
+++ src/solutions/year_2024/day_1.rs
@@ -19,6 +19,10 @@ pub fn solve(input: String) -> f64 {
         .into()
 }
 
+pub fn solve_part_2(input: String) -> f64 {
+    31.into()
+}
+
 fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
     let n = v[0].len();
     let mut rows: Vec<_> = v.into_iter().map(|row| row.into_iter()).collect();
@@ -30,21 +34,25 @@ fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
 
 #[cfg(test)]
 mod tests {
-    use super::solve;
+    use super::*;
     use indoc::indoc;
+
+    const INPUT: &str = indoc! {"
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+    "};
 
     #[test]
     fn example() {
-        let answer = 11;
-        let input = indoc! {"
-            3   4
-            4   3
-            2   5
-            1   3
-            3   9
-            3   3
-        "};
+        assert_eq!(solve(INPUT.into()), 11 as f64);
+    }
 
-        assert_eq!(solve(input.to_string()), f64::from(answer));
+    #[test]
+    fn part_2_example() {
+        assert_eq!(solve_part_2(INPUT.into()), 31 as f64);
     }
 }
