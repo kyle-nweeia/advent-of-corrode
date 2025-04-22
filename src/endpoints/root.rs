256070533bb306808c10eb6f948ad038c6f1033f
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-04-22 11:44:58 -0600

    Serve index.html

--- src/endpoints/root.rs
+++ src/endpoints/root.rs
@@ -1,41 +1,5 @@
-use axum::{response::Html, routing::Router};
-
-async fn handler() -> Html<&'static str> {
-    Html(
-        r#"
-        <!doctype html>
-        <html>
-            <head>
-                <script>
-                    function setAction(form) {
-                        form.action = `/submit/${form.year.value}/${form.day.value}/${form.part.value}`;
-                    }
-                </script>
-            </head>
-            <body>
-                <form>
-                    <div>
-                        <label>Year:</label>
-                        <input name="year" onchange="setAction(form)" type="text">
-                    </div>
-                    <div>
-                        <label>Day:</label>
-                        <input name="day" onchange="setAction(form)" type="text">
-                    </div>
-                    <div>
-                        <label>Part:</label>
-                        <input name="part" onchange="setAction(form)" type="text">
-                    </div>
-                    <div>
-                        <input type="submit">
-                    </div>
-                </form>
-            </body>
-        </html>
-        "#,
-    )
-}
+use axum::routing::Router;
 
 pub fn router() -> Router {
-    Router::new().route("/", axum::routing::get(handler))
+    Router::new().fallback_service(tower_http::services::ServeDir::new("assets"))
 }
