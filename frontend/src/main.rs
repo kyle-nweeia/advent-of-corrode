79514a0f2557b960d3a19ccc7400b58783d1a5f9
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-04-30 18:39:45 -0600

    Destructure form values

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,20 +4,35 @@ fn main() {
     dioxus::launch(app);
 }
 
+struct FormValues<'a> {
+    year: &'a str,
+    day: &'a str,
+    part: &'a str,
+}
+
+impl<'a> FormValues<'a> {
+    fn from(vals: &'a std::collections::HashMap<String, FormValue>) -> Option<FormValues<'a>> {
+        Some(FormValues {
+            year: vals.get("year")?.first()?,
+            day: vals.get("day")?.first()?,
+            part: vals.get("part")?.first()?,
+        })
+    }
+}
+
 fn app() -> Element {
     rsx! {
         form {
             onsubmit: async |evt: FormEvent| {
-                let year = evt.values()["year"].as_value();
-                let day = evt.values()["day"].as_value();
-                let part = evt.values()["part"].as_value();
-                let result = reqwest::Client::new()
-                    .get(format!("http://localhost:3000/submit/{year}/{day}/{part}"))
-                    .send()
-                    .await;
-                if let Ok(response) = result {
-                    if let Ok(text) = response.text().await {
-                        dioxus_logger::tracing::info!("{text:?}");
+                if let Some(FormValues { year, day, part }) = FormValues::from(&evt.values()) {
+                    let result = reqwest::Client::new()
+                        .get(format!("http://localhost:3000/submit/{year}/{day}/{part}"))
+                        .send()
+                        .await;
+                    if let Ok(response) = result {
+                        if let Ok(text) = response.text().await {
+                            dioxus_logger::tracing::info!("{text:?}");
+                        }
                     }
                 }
             },
