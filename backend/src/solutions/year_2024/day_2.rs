9f76c64b69c198ae977cfc481e151b1320c0b85f
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-05-22 19:36:34 -0600

    Add 2024/2

---
+++ backend/src/solutions/year_2024/day_2.rs
@@ -0,0 +1,43 @@
+use crate::endpoints::solution::Part;
+
+pub fn solve(part: Part) -> super::Solver {
+    match part {
+        Part::One => solve_part_1::<String>,
+        Part::Two => solve_part_2::<String>,
+    }
+}
+
+pub fn solve_part_1<T>(input: T) -> f64
+where
+    String: From<T>,
+{
+    todo!();
+}
+
+pub fn solve_part_2<T>(input: T) -> f64
+where
+    String: From<T>,
+{
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    const INPUT: &str = indoc! {"
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+    "};
+
+    #[test]
+    fn part_1_example() {}
+
+    #[test]
+    fn part_2_example() {}
+}
--- backend/src/solutions/year_2024/mod.rs
+++ backend/src/solutions/year_2024/mod.rs
@@ -5,10 +5,12 @@ use crate::endpoints::solution::Part;
 use super::Solver;
 
 mod day_1;
+mod day_2;
 
 pub fn get_solution(day: u32, part: Part) -> Result<Solver, StatusCode> {
     match day {
         1 => Ok(day_1::solve(part)),
+        2 => Ok(day_2::solve(part)),
         _ => Err(StatusCode::NOT_FOUND),
     }
 }
